061fb113e9e9b22de98e313c54beda40ef785134
Sascha Wolf <swolf.dev@gmail.com>
2013-07-15 13:14:06 +0200

    Minor Bugfix in data.js. Darstellung ist nun korrekt, wenn der Server neu gestartet wird. Keine multiplen Eintraege mehr.
    Conflicts:
    	src/public/js/data/data.js

--- src/public/js/data/data.js
+++ src/public/js/data/data.js
@@ -16,35 +16,38 @@ $(document).ready(function() {
 		return data.pop().values;
 	}
 
-	var socket = io.connect('http://'+window.location.host+'/data')
-			// The first data
-			.on('first', function(data) {
-				var values = getValues(data);
-				if(values === undefined) return;
+	var socket = io.connect('http://'+window.location.host+'/data');
 
-				// Create the table
-				for(var i=0; i<values.length; ++i) {
-					$('#namen').append('<h3 class="subheader">Messwert '+(i+1)+':</h3>');
-					$('#werte').append('<h3 id="value'+i+'">'+values[i]+'</h3>');
-				}
+	// The first data
+	socket.on('first', function(data) {
+		var values = getValues(data);
+		if(values === undefined) return;
 
-				// Hide the loading message
-				$('#load').fadeOut(undefined, function() {
-					// Show the data
-					$('#data').fadeIn();
-				});
-			})
-			.on('data', function(data) {
-				var values = getValues(data);
-				if(values === undefined) return;
+		// Clear the area
+		$('#namen').empty();
+		$('#werte').empty();
 
-				// Change the values
-				for(var i=0; i<values.length; ++i) {
-					$('#value'+i).text(values[i]);
-				}
-			});
+		// Create the table
+		for(var i=0; i<values.length; ++i) {
+			$('#namen').append('<h3 class="subheader">Messwert '+(i+1)+':</h3>');
+			$('#werte').append('<h3 id="value'+i+'">'+values[i]+'</h3>');
+		}
 
+		// Hide the loading message
+		$('#load').fadeOut(undefined, function() {
+			// Show the data
+			$('#data').fadeIn();
+		});
+	});
+	socket.on('data', function(data) {
+		var values = getValues(data);
+		if(values === undefined) return;
 
+		// Change the values
+		for(var i=0; i<values.length; ++i) {
+			$('#value'+i).text(values[i]);
+		}
+	});
 
 	// Public stuff
 	return {
