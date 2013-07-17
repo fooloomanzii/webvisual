ac165301d1ebba828e1ac3eaf0eceb8b2246dba5
Sascha Wolf <swolf.dev@gmail.com>
2013-07-17 17:27:47 +0200

    buster.js Update. Funktional fuer tests.

--- src/test/buster.js
+++ src/test/buster.js
@@ -1,24 +1,28 @@
 var config = module.exports;
 
-config["Browser tests"] = {
-	environment: "browser",
-	rootPath: "../",
-	libs: [
-		"public/js/jquery-2.0.2.js",
-		"public/js/foundation/*.js",
-		"public/js/vendor/zepto.js",
-		"public/js/vendor/custom.modernizr.js",
-		"public/js/rgraph/RGraph*.js",
-		"public/js/moment/moment.js"
-	],
-	src: [
-		"public/js/graphs/**/*.js",
-		"public/js/*/*.js"
-	],
-	tests: ["test/browser/*test.js"]
-};
+// config["Default"] = {
+// };
+
+// config["Browser tests"] = {
+// 	// extends: "Default",
+// 	environment: "browser",
+// 	libs: [
+// 		"public/js/jquery-2.0.2.js",
+// 		"public/js/foundation/*.js",
+// 		"public/js/vendor/zepto.js",
+// 		"public/js/vendor/custom.modernizr.js",
+// 		"public/js/rgraph/RGraph*.js",
+// 		"public/js/moment/moment.js"
+// 	],
+// 	src: [
+// 		"public/js/graphs/**/*.js",
+// 		"public/js/*/*.js"
+// 	],
+// 	tests: ["browser/*-test.js"]
+// };
 
 config["Node tests"] = {
+	// extends: "Default",
 	environment: "node",
-	tests: ["test/node/*test.js"]
+	tests: ["node/*-test.js"]
 };
