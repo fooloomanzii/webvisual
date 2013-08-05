5e44a4b0ceadbb7b433f63726fe633ffee7901b3
Sascha Wolf <swolf.dev@gmail.com>
2013-08-05 21:30:45 +0200

    npm benoetigt Patch-Versionsnummer. npm install funktioniert sonst nicht.

--- bump-version.cmd
+++ bump-version.cmd
@@ -2,4 +2,4 @@
 
 set version="%1"
 
-node -e "var fs=require('fs'),path='./src/package.json',pack=require(path),version="%version%";function isVersionNumber(a){return(/^\d{1,2}\.\d{1,2}(\.\d{1,2}[a-z]?)?$/.test(a))}if(!isVersionNumber(version)){console.error('No valid version number.');process.exit(1)};pack.version=version;fs.writeFileSync(path,JSON.stringify(pack,null,'\t'));console.log('Version number '+version+' was written into '+path+'.');"
+node -e "var fs=require('fs'),path='./src/package.json',pack=require(path),version="%version%";function isVersionNumber(a){return(/^\d{1,2}\.\d{1,2}\.\d{1,2}[a-z]?$/.test(a))}if(!isVersionNumber(version)){console.error('No valid version number.');process.exit(1)};pack.version=version;fs.writeFileSync(path,JSON.stringify(pack,null,'\t'));console.log('Version number '+version+' was written into '+path+'.');"
