aae8e5b70a503bc205a160a8f28944a7e4f15f8c
Sascha Wolf <swolf.dev@gmail.com>
2013-08-22 15:32:16 +0200

    Misc: Run.bat updated. Sorgt nun dafuer, dass eine data.txt vorhanden ist.

--- src/run.bat
+++ src/run.bat
@@ -1,7 +1,14 @@
 @echo off
 
-if not exist .\node_modules goto INSTALL
+if not exist .\node_modules call :INSTALL
+if not exist .\data.txt call :DATA
 goto START
+
+:DATA
+(echo 13.06.2013,10:20:08,3.676,5.297,3.907
+echo 13.06.2013,10:20:09,2.280,9.223,6.819
+<nul set /p =13.06.2013,10:20:10,5.345,2.382,0.770) > data.txt
+goto:eof
 
 :INSTALL
 echo Install packages. This can take a moment ...
@@ -9,9 +16,11 @@ start /B /WAIT cmd /C "npm install" >NUL 2>&1
 echo.
 echo Packages installed. Start server now.
 echo.
+goto:eof
 
 :START
 title Messdatenvisualierung (Server)
 node app.js
 
 PAUSE
+goto:eof
