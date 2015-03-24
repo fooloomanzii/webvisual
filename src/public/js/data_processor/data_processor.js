5e54885fbd63cb684b7ea3addc9abc46aa485175
dagnarus <diener.alexander@live.de>
2015-03-24 09:46:33 +0100

    Translated Comments in the data_processor.js

--- src/public/js/data_processor/data_processor.js
+++ src/public/js/data_processor/data_processor.js
@@ -1,130 +1,130 @@
 (function(){
 
-  // globale Variablen
-      numCols = 2,        // aus config/config.json           // Anzahl der Spalten der Messdaten
-      labelsArray = [],   // aus config/config.json           // Array für die Labels
-      valuesArray = [],   // aus Datendatei (z.Z. data.txt)   // Array für die Messdaten
-      exceedsArray = [],  // wird berechnet (boolean Werte)   // Array für die Über- und Unterschreitungen der Messwerte
-      dateArray = [];     // wird ausgelesen (in Date-Format) // Array der Messzeitpunkte (vgl. Zeilen der Messdatei)
-      combinedData = [];  // wird erstellt                    // Zusammenlegung der Daten
+  // global Variables
+      numCols = 2,        // from config/config.json             // Column Number for Data Values
+      labelsArray = [],   // from config/config.json             // Labels Array
+      valuesArray = [],   // from Datafile (z.Z. data.txt)       // Data Values Array
+      exceedsArray = [],  // will be calculated (boolean Values) // Array for the Exceeds
+      dateArray = [];     // will be readout (in Date-Format)    // Array for Timestamps (cf. Lines of Datafile)
+      combinedData = [];  // will be created                     // Zusammenlegung der Daten
 
-    // Beispieltabelle aus den Daten:
+    // Data Examples:
     //
-    // Raum          Label        x1      x2    ... (Spalten der Messdaten ist numCols)  exc_x1  exc_x2  ...
-    // (roomArray)   (labelArray) (valuesArray)                                          (exceedsArray)
+    // Room          Label        x1      x2    ... (Columns of the Data are numCols)  exc_x1  exc_x2  ...
+    // (roomArray)   (labelArray) (valuesArray)                                        (exceedsArray)
     //
-    // Raum A        Sauerstoff   3.4     4.5   ...                                      false   false
-    // Raum B        Chlor        4.2     2.3   ...                                      true    false
-    // Raum A        Chlor        6.2     7.3   ...                                      true    true
-    // ....          ....         ...     ...                                            ..      ..
+    // Raum A        Sauerstoff   3.4     4.5   ...                                    false   false
+    // Raum B        Chlor        4.2     2.3   ...                                    true    false
+    // Raum A        Chlor        6.2     7.3   ...                                    true    true
+    // ....          ....         ...     ...                                          ..      ..
 
 
-    // weitere erzeugte Elemente in der Client-Umgebung:
+    // further created Elements in the Client-Environment
 
-    // #lastRTime     Zeitlabel für letzte erfolgreiche Datenübermittlung
-    // #lastWTime     Zeitlabel für letzte nicht erfolgreiche Datenübermittlung
+    // #lastRTime     Label for the Time of the last successful Data Transmission
+    // #lastWTime     Label for the Time of the last unsuccessful Data Transmission
 
-    // #dataTimeLabel Bezeichner für "letzte Nachricht"
-    // #load          Lademeldung
+    // #dataTimeLabel Label for the Time of the last Message
+    // #load          Loading notice
 
 
-  // Start des Ladevorgangs
-  // (wenn der Client bereit ist)
+  // Start of the Loading Process
+  // (if the Client is ready)
     $(document).ready(function() {
 
-      // Start der Serververbindung zu der Konfigurationsdatei
-      // (z.Z. "config/config.json")
+      // Start of the Server Connection to the Config file
+      // (at present "config/config.json")
       var configSocket = io.connect('http://'+window.location.host+'/config');
       var dataSocket = null;
 
-      //***** Empfangen der Daten und Konfigurationen
+      //***** Receive of Configuration Data
       configSocket.on('data', function(message) {
-        if(message === undefined) return; // Prüfung auf Existenz
+        if(message === undefined) return; // Check the Existence
 
-        // Funktionsaufruf: Auslesen der Konfigurationsdatei
+        // Function call: Read the Config file
         arrangeLabels(message.locals);
 
-        // Verbindungsaufbau zur Datendatei
-        // (wenn eine Konfigurationsdatei gelesen werden konnte, siehe oben)
+        // Establish connection to the Data File
+        // (if a Config File was read, see above)
         dataSocket = io.connect('http://'+window.location.host+'/data');
 
-        // Wartestatus
+        // Waiting Status
         dataSocket.on('wait', function() {
-          // (Lademeldung) #load
+          // (Loading Notification) #load
           $('#load').text("Lade Daten...");
         });
 
-        //*** Empfang der ersten Daten
-        // (siehe copywatch?)
+        //*** Receiving the first Data
+        // (look at copywatch/udpwatch?)
         dataSocket.on('first', function(message) {
-          if(message === undefined) return; // Prüfung auf Existenz
+          if(message === undefined) return; // Check the Existence
 
-          // Setzen des Zeitlabels '#lastRTime'
-          // (für letzte erfolgreich empfangene Daten)
+          // Set the Timelabel '#lastRTime'
+          // (for the last successful received Data)
           $('#lastRTime').text(
               $.format.toBrowserTimeZone(message.time,labelsArray.timeFormat));
 
-          // Funktionsaufruf: Anordnung der Daten
+          // Function Call: Data Arrangement
           arrangeData(message.data, message.exceeds);
 
-          // Ausblenden der Lademeldung
+          // Hide the Loading Notification
           $('#load').fadeOut(undefined, function() {
-            // Anzeige in einer Progressbar
+            // Displaying in a Progressbar
           });
         });
 
-        //*** Empfang weiterer Daten
+        //*** Receive another Data
         dataSocket.on('data', function(message) {
-          if(message === undefined) return; // Prüfung auf Existenz
+          if(message === undefined) return; // Check for Existence
 
-          // Setzen des Zeitlabels '#lastRTime'
-          // (für letzte erfolgreich empfangene Daten)
+          // Set the Timelabel '#lastRTime'
+          // (for the last successful received Data)
           $('#lastRTime').text(
               $.format.toBrowserTimeZone(message.time,labelsArray.timeFormat));
 
-          // Funktionsaufruf: Erneuerung der Daten
+          // Function Call: Data Renewing
           arrangeData(message.data, message.exceeds);
         });
 
-        //*** Fehlerhafte Daten
+        //*** Wrong Data
         dataSocket.on('mistake', function(message) {
-          // Setzen des Zeitlabels '#lastWTime'
-          // (für letzte nicht erfolgreich empfangene Daten)
+          // Set the Timelabel '#lastWTime'
+          // (for the last unsuccessful received Data)
           $('#lastWTime').text(
               $.format.toBrowserTimeZone(message.time,labelsArray.timeFormat));
         });
       });
 
-      //***** Rückgabe von Werten für public?
+      //***** Return of the Data for public?
       return {
         socket: dataSocket
       };
     });
 
 
-  //***** Laden der Konfiguration in das Datenobjekt
+  //***** Load the Configuration in the Data Object
   function arrangeLabels(locals) {
-    if (!locals) {
-      throw new Error("Keine Konfiguration vorhanden"); // Prüfung auf Existenz
+    if (!locals) { // Check for Existence
+      throw new Error("Keine Konfiguration vorhanden"); 
       return;
     }
     else {
       labelsArray=locals;
     }
 
-    // Setzen des Bezeichners für "letzte Nachricht" '#dataTimeLabel'
+    // Set the Title for the '#dataTimeLabel' of the last Message
     $('#dataTimeLabel').text(locals.dataTimeLabel);
-    // Setzen des Bezeichners für "letzte Nachricht" '#dataTimeLabel'
+    // Set the '#dataTime' of the last Message
     $('#dataTime').text("");
 
-    // Setzen der Anzahl der verschiedenen Variablen bzw. Spalten 'numCols'
+    // Set the Number of different Variables and Columns 'numCols' respectively 
     numCols = locals.typeWidth;
 
     if (numCols < 1) {
       numCols = 1;
     }
 
-    // Erzeugen der Labels für die Wertetabelle 'subtypes'
+    // Create labels for the Value Table 'subtypes'
     for(var j = 0; j<locals.types.lenght; j++){
       for(var i = 1; i<=numCols; i++){
         if (!locals.types[j].subtypes[i-1]) {
@@ -135,18 +135,18 @@
     }
   }
 
-  //***** Anordnung der Daten
+  //***** Data Arrangement
   function arrangeData(data, exceeds){
-    if(!data || data.length == 0 ) return;  // Prüfung auf Existenz
+    if(!data || data.length == 0 ) return;  // Check for Existence
 
-    // Setzen des Über- oder Unterschreitungsarrays 'exceedsArray'
+    // Set the Arrays of Exceeds 'exceedsArray'
     var exceedsArray = exceeds;
 
-    // Lesen der letzten Zeile der Messdatei
+    // Read the last Line of the Data File
 
-    // Setzen des Datenarrays 'valuesArray' und Messzeitarray 'dateArray'
+    // Set the Array of Values 'valuesArray' and Array of Timestamps 'dateArray'
     for (var i=0; i<data.length; i++) {
-        // Wenn schon Daten existieren, werden diese überschrieben
+      // If that Data exists, it will be overwritten
       if(valuesArray[i] && dateArray[i]) {
         for (var k=0; k<data[i].values.length; k++) {
           valuesArray[i][k] = data[i].values[k];
@@ -154,15 +154,15 @@
         dateArray[i] = $.format.toBrowserTimeZone(
             data[i].date,labelsArray.timeFormat);
       }
-        // Ansonsten, werden die Daten an die Arrays angefügt
+      // Otherwise: append the Data to that Arrays
       else {
         valuesArray.push(data[i].values);
         dateArray.push($.format.toBrowserTimeZone(
             data[i].date,labelsArray.timeFormat));
       }
 
-      // Erzeugen der Labels für die Typentabelle 'types', wenn keine Labels in der 'config.json' definiert sind
-      // (Raum 'room' & Typ der Messung oder andere Bezeichnung 'kind')
+      // Create the Labels for the Type Table 'types', if no Labels are defined in 'config.json'
+      // (Room 'room' & Type of the Measurement or other Labels 'kind')
       for(var j = 1; j <= (data[i].values.length / numCols); j++){
         if (!labelsArray.types[j-1]) {
           labelsArray.types.push(
@@ -175,7 +175,7 @@
       }
     }
 
-    // 1. Zusammenführung in 'combinedData'
+    // 1. Junction in 'combinedData'
     combinedData = {labels: labelsArray.types, colors: labelsArray.colors, data: []}
     for (var i=0; i<data.length; i++) {
       combinedData.data.push({"date" : dateArray[i],
@@ -183,7 +183,7 @@
                               "exceeds" : exceedsArray[i]});
     }
 
-    // 2. Erzeugen eines einzigen Arrays mit allen Messpunkten
+    // 2. Creation of a single Array with all Data Values
     var dataStringArray = [];
     for (var i=0; i<data.length; i++) {
       for (var j=0; j<labelsArray.types.length; j++) {
@@ -211,11 +211,11 @@
         }
       }
     }
-      // erzeugen eines Objects, damit es dem Eventhandler zugeordnet werden kann (dirty)
+      // Creation of an Object, so it can be assigned to the Eventhandler (dirty)
     var dataStringObject = {content: dataStringArray};
 
-    // Triggering von einem Event zum Document 'dataLoaded', um zu zeigen,
-    // dass die Daten bereit sind und die Daten zu übergeben
+    // Triggering of an Event for the Document 'dataLoaded' to show,
+    // that Data Values are ready for the delivery
     $(document).triggerHandler("dataLoaded", dataStringObject);
   }
 
