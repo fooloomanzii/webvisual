0f3b2731c392d907ed7692f70cfa529ea1aae77e
Fairyon <dagnarus@live.ru>
2015-07-13 18:10:27 +0200

    some little db changes (after that comes new branch)

--- src/modules/datadb/lib/dbcontroller.js
+++ src/modules/datadb/lib/dbcontroller.js
@@ -22,7 +22,7 @@ DataController.prototype.appendData = function (newData, callback) {
         }));
       }
           
-      if (numberAffected === 1) { // Success
+      if (numberAffected === 1) { // Successful
         var datamodel = new me.datamodel({
           date = user.date,
           id = user.id,
@@ -34,10 +34,6 @@ DataController.prototype.appendData = function (newData, callback) {
           isBoolean = user.isBoolean,
           threshold = user.threshold,
           unit = user.unit
-          
-            email: user.email,
-            firstName: user.firstName,
-            lastName: user.lastName
         });
 
         return callback(err, new me.ApiResponse({
@@ -52,168 +48,11 @@ DataController.prototype.appendData = function (newData, callback) {
       }             
   
   });
-  
-  
-  me.userModel.findOne({ id: newData.id }, function (err, user) {
-    if (err) {
-      return callback(err, new me.ApiResponse({ success: false, extras: { msg: me.ApiMessages.DB_ERROR } }));
-    }
-    
-    if (user) {
-      return callback(err, new me.ApiResponse({ success: false, extras: { msg: me.ApiMessages.EMAIL_ALREADY_EXISTS } }));
-    } else {
-    
-      newUser.save(function (err, user, numberAffected) {
-  
-          if (err) {
-              return callback(err, new me.ApiResponse({ success: false, extras: { msg: me.ApiMessages.DB_ERROR } }));
-          }
-              
-          if (numberAffected === 1) {
-  
-              var userProfileModel = new me.UserProfileModel({
-                  email: user.email,
-                  firstName: user.firstName,
-                  lastName: user.lastName
-              });
-  
-              return callback(err, new me.ApiResponse({
-                  success: true, extras: {
-                      userProfileModel: userProfileModel
-                  }
-              }));
-          } else {
-              return callback(err, new me.ApiResponse({ success: false, extras: { msg: me.ApiMessages.COULD_NOT_CREATE_USER } }));
-          }             
-  
-      });
-    }
-  }
 }
 
-AccountController.prototype.register = function (newUser, callback) {
-  var me = this;
-  me.userModel.findOne({ email: newUser.email }, function (err, user) {
-
-      if (err) {
-          return callback(err, new me.ApiResponse({ success: false, extras: { msg: me.ApiMessages.DB_ERROR } }));
-      }
-
-      if (user) {
-          return callback(err, new me.ApiResponse({ success: false, extras: { msg: me.ApiMessages.EMAIL_ALREADY_EXISTS } }));
-      } else {
-
-          newUser.save(function (err, user, numberAffected) {
-
-              if (err) {
-                  return callback(err, new me.ApiResponse({ success: false, extras: { msg: me.ApiMessages.DB_ERROR } }));
-              }
-                  
-              if (numberAffected === 1) {
-
-                  var userProfileModel = new me.UserProfileModel({
-                      email: user.email,
-                      firstName: user.firstName,
-                      lastName: user.lastName
-                  });
-
-                  return callback(err, new me.ApiResponse({
-                      success: true, extras: {
-                          userProfileModel: userProfileModel
-                      }
-                  }));
-              } else {
-                  return callback(err, new me.ApiResponse({ success: false, extras: { msg: me.ApiMessages.COULD_NOT_CREATE_USER } }));
-              }             
-
-          });
-      }
-
-  });
-};
-
-
-DataController.prototype.logon = function(email, password, callback) {
-
-  var me = this;
-
-  me.userModel.findOne({ email: email }, function (err, user) {
-
-      if (err) {
-          return callback(err, new me.ApiResponse({ success: false, extras: { msg: me.ApiMessages.DB_ERROR } }));
-      }
-
-      if (user) {
-
-          me.hashPassword(password, user.passwordSalt, function (err, passwordHash) {
-
-              if (passwordHash == user.passwordHash) {
-
-                  var userProfileModel = new me.UserProfileModel({
-                      email: user.email,
-                      firstName: user.firstName,
-                      lastName: user.lastName
-                  });
-
-                  me.session.userProfileModel = userProfileModel;
-
-                  return callback(err, new me.ApiResponse({
-                      success: true, extras: {
-                          userProfileModel:userProfileModel
-                      }
-                  }));
-              } else {
-                  return callback(err, new me.ApiResponse({ success: false, extras: { msg: me.ApiMessages.INVALID_PWD } }));
-              }
-          });
-      } else {
-          return callback(err, new me.ApiResponse({ success: false, extras: { msg: me.ApiMessages.EMAIL_NOT_FOUND } }));
-      }
-
-  });
-};
-
-
-
-DataController.prototype.logon = function(email, password, callback) {
-
-  var me = this;
-
-  me.userModel.findOne({ email: email }, function (err, user) {
-
-      if (err) {
-          return callback(err, new me.ApiResponse({ success: false, extras: { msg: me.ApiMessages.DB_ERROR } }));
-      }
-
-      if (user) {
-
-          me.hashPassword(password, user.passwordSalt, function (err, passwordHash) {
-
-              if (passwordHash == user.passwordHash) {
-
-                  var userProfileModel = new me.UserProfileModel({
-                      email: user.email,
-                      firstName: user.firstName,
-                      lastName: user.lastName
-                  });
-
-                  me.session.userProfileModel = userProfileModel;
-
-                  return callback(err, new me.ApiResponse({
-                      success: true, extras: {
-                          userProfileModel:userProfileModel
-                      }
-                  }));
-              } else {
-                  return callback(err, new me.ApiResponse({ success: false, extras: { msg: me.ApiMessages.INVALID_PWD } }));
-              }
-          });
-      } else {
-          return callback(err, new me.ApiResponse({ success: false, extras: { msg: me.ApiMessages.EMAIL_NOT_FOUND } }));
-      }
-
-  });
-};
+DataController.prototype.getAllData = function (callback) {
+  callback(this.datamodel.find().pretty());
+}
 
 
 module.exports = DataController;
