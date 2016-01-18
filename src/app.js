67edc02d2d1644663573ac58546702f68012e271
fooloomanzii <hannes-b@web.de>
2016-01-18 20:26:06 +0100

    routing and auth
    instable

--- src/app.js
+++ src/app.js
@@ -3,74 +3,76 @@
  * Module dependencies
  */
 var // EXPRESS
-    express      = require('express'),
+    express = require('express'),
     // EXPRESS-ERROR-HANDLER
     errorHandler = require('express-error-handler'),
     // X-FRAME-OPTIONS <-- prevent Clickjacking
     xFrameOptions = require('x-frame-options'),
+
+    bodyParser = require('body-parser'),
     // FS <-- File System
-    fs           = require('fs'),
+    fs = require('fs'),
     // UNDERSCORE <-- js extensions
-    _            = require('underscore'),
+    _ = require('underscore'),
     // DEFAULTSDEEP <-- extended underscrore/lodash _.defaults,
     // for default-value in   deeper structures
     defaultsDeep = require('merge-defaults'),
-    // custom: ROUTING
-    routes       = require('./routes'),
     // DATA-MODULE
-    dataModule   = require('./data_module'),
+    dataModule = require('./data_module'),
     /* Default config */
-    defaults     = { connections     : [],
-                     port            : 3000,
-                     updateIntervall : 1000,
-                     dbName          : "test"},
+
+    defaults =
+        {
+          connections : [],
+          port : 3000,
+          updateIntervall : 1000,
+          dbName : "test"
+        },
     // Config Object
     config,
     // Logger
     winston = require('winston');
 
-
 try {
   config = JSON.parse(fs.readFileSync(__dirname + '/config/config.json'));
-}
-catch (err) {
+} catch (err) {
   console.warn('There has been an error parsing the config-file.')
-  console.warn(err.stack);
+      console.warn(err.stack);
 }
 
 // Use defaults for undefined values
-config = defaultsDeep(config,defaults);
+config = defaultsDeep(config, defaults);
 
 /*
  * Extend UNDERSCORE
  */
 _.mixin({
-  exclone: function(object, extra) {
+  exclone : function(object, extra) {
     return _(object).chain().clone().extend(extra).value();
   }
 });
 
-//Checks for program arguments and runs the responsible operations
-//e.g. "node app.js <arg1> <arg2>"
+// Checks for program arguments and runs the responsible operations
+// e.g. "node app.js <arg1> <arg2>"
 //   "-port <portnumber>" changes server port on <portnumber>
-function checkArguments(){
-  for(var i=0; i<process.argv.length; i++){
-   switch(process.argv[i]) {
-     case "-port": // next argument need to be a port number
-       // isNaN() checks if content is not a number
-       if(!isNaN(process.argv[++i])){
-           config.port = process.argv[i];
-       } else {// next argument isn't a port number, so check it in next loop
-         i--;
-       }
-       break;
-     default:
-     // react on unrecognized arguments
-   }
+function checkArguments() {
+  for (var i = 0; i < process.argv.length; i++) {
+    switch (process.argv[i]) {
+    case "-port": // next argument need to be a port number
+      // isNaN() checks if content is not a number
+      if (!isNaN(process.argv[++i])) {
+        config.port = process.argv[i];
+      } else { // next argument isn't a port number, so check it in next loop
+        i--;
+      }
+      break;
+    default:
+      // react on unrecognized arguments
+    }
   }
 }
 
-//check for program arguments
+// check for program arguments
 checkArguments();
 
 /*
@@ -81,96 +83,101 @@ checkArguments();
 var sslOptions  = {
     key: fs.readFileSync(__dirname + '/ssl/ca.key', 'utf8'),
     cert: fs.readFileSync(__dirname + '/ssl/ca.crt', 'utf8'),
-	ca: [ // Files for the certification path
-            fs.readFileSync(__dirname + '/ssl/ca_DFN.crt', 'utf8'),
-            fs.readFileSync(__dirname + '/ssl/ca_FZJ.crt', 'utf8')
-         ],
+	  ca: [ // Files for the certification path
+          fs.readFileSync(__dirname + '/ssl/ca_DFN.crt', 'utf8'),
+          fs.readFileSync(__dirname + '/ssl/ca_FZJ.crt', 'utf8')
+        ],
     passphrase: require('./ssl/ca.pw.json').password,
     requestCert: true,
     rejectUnauthorized: false
   };
 
 // General
-var app    = express(),
+var app = express(),
     server = require('https').createServer(sslOptions, app);
 
 // Prevent Clickjacking
 app.use(xFrameOptions());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//   extended: true
+// }));
 
 // Path to static folder
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public/'));
 
-// Configure VIEW ENGINE
-app.set('view engine', 'jade');
-app.set('views', __dirname + '/views');
+// handling ENVIRONMENTS
+// TODO: find better seperation, what to do in differnet production environments
+// Development
+if (app.get('env') == 'development') {
+  // Make the Jade output readable
+  app.locals.pretty = true;
+  // Make the Jade output readable and add the environment specification
+  _.extend(app.locals, {
+    env : 'development',
+    pretty : true,
+  });
+}
+// Production
+if (app.get('env') == 'production') {
+  // Set the environment specification for jade
+  app.locals.env = 'production';
+}
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routing
+var routes = require('./routes')(app);
 
 /*
  * Defining Errors
  */
 
 // if Error: EADDRINUSE --> log in console
-server.on('error', function (e) {
-    if (e.code == 'EADDRINUSE') {
-      console.log('Port '+config.port+' in use, retrying...');
-      console.log("Please check if \"node.exe\" is not already running on this port.");
-      setTimeout(function () {
-        if(running) server.close();
-        server.listen(config.port);
-      }, 1000);
-    }
-  })
-  .once('listening', function() {
-    console.log("Server is running under Port %d in %s mode",
-        config.port, app.settings.env);
-  });
-
-  // handling ENVIRONMENTS
-  // TODO: find better seperation, what to do in differnet production environments
-  // Development
-  if ( app.get('env') == 'development' ) {
-    // Make the Jade output readable
-    app.locals.pretty = true;
-    // Make the Jade output readable and add the environment specification
-    _.extend(app.locals, {
-      env:    'development',
-      pretty: true,
+server
+    .on('error',
+        function(e) {
+          if (e.code == 'EADDRINUSE') {
+            console.log('Port ' + config.port + ' in use, retrying...');
+            console.log(
+                "Please check if \"node.exe\" is not already running on this port.");
+            setTimeout(function() {
+              if (running)
+                server.close();
+              server.listen(config.port);
+            }, 1000);
+          }
+        })
+    .once('listening', function() {
+      console.log("Server is running under Port %d in %s mode", config.port,
+                  app.settings.env);
     });
-  }
-  // Production
-  if ( app.get('env') == 'production' ) {
-    // Set the environment specification for jade
-    app.locals.env = 'production';
-  }
 
 /*
  * Routing (./routes/index.js)
  */
 
 // Route: Default, Home, Index
-app.get('/', routes.index);
+// app.get('/', routes.login);
 
 // Route: External Log File
-app.get('/log', routes.externalLogFile);
+// app.get('/authenticate', routes.authenticate);
 
-// Route: Data File
-app.get('/data', routes.dataString);
+// // Route: External Log File
+// app.get('/log', routes.externalLogFile);
+//
+// // Route: Data File
+// app.get('/data', routes.dataString);
+//
+// // Route: Data File
+// app.get('/settings', routes.settingsJSON);
 
-// Route: Data File
-app.get('/settings', routes.settingsJSON);
+// routes.authenticate({username: "ibn-net\\j.brautzsch", password: "(jab123)"});
 
-
-// Error: Custom 404 page
-app.use(function(req, res) {
-  res.status(404);
-
-  // Respond with html page
-  if(req.accepts('html')) {
-    res.render('404');
-  }
-});
 
 // connect the DATA-Module
-dataModule.connect(config,server);
+dataModule.connect(config, server);
 
 /*
  * Handle various process events
@@ -178,28 +185,27 @@ dataModule.connect(config,server);
 
 // After Server has started, we start the winston.logger
 var logger = new winston.Logger({
-  transports: [
-   new (winston.transports.Console)()
+  transports : [ new (winston.transports.Console)() ],
+  exceptionHandlers : [
+    new winston.transports.File(
+        {filename : __dirname + config.logs.server_log})
   ],
-  exceptionHandlers: [
-   new winston.transports.File({ filename: __dirname + config.logs.server_log })
-  ],
-  exitOnError: false
+  exitOnError : false
 });
 
- // TODO: make sure that the server is not closing (or is restarting) with errors
- // and pretty this part
+// TODO: make sure that the server is not closing (or is restarting) with errors
+// and pretty this part
 process.on('uncaughtException', function(err) {
   logger.log('error', err); // print error to the logger (console + file)
   try {
     server.close();
     dataModule.disconnect();
   } catch (e) {
-    if(e.message !== 'Not running')
+    if (e.message !== 'Not running')
       throw e;
   }
-  //try to reconnect
-  //dataModule.connect(config,server);
+  // try to reconnect
+  // dataModule.connect(config,server);
 });
 
 process.on('ECONNRESET', function(err) {
@@ -207,14 +213,13 @@ process.on('ECONNRESET', function(err) {
     // server.close();
     // dataModule.disconnect();
   } catch (e) {
-    if(e.message !== 'Not running')
+    if (e.message !== 'Not running')
       throw e;
   }
   // try to reconnect
-  console.error('ECONNRESET: '+err);
-  //dataModule.connect(config,server);
+  console.error('ECONNRESET: ' + err);
+  // dataModule.connect(config,server);
 });
-
 
 /* SIGINT can usually be generated with Ctrl-C */
 process.on('SIGINT', function(err) {
@@ -224,10 +229,10 @@ process.on('SIGINT', function(err) {
     console.log('disconnect db');
     dataModule.disconnect();
   } catch (err) {
-    if(err.message !== 'Not running')
+    if (err.message !== 'Not running')
       throw err;
   }
-  
+
   console.warn(err.stack);
 });
 
@@ -236,8 +241,9 @@ process.on('exit', function(err) {
     server.close();
     dataModule.disconnect();
   } catch (err) {
-    if(err.message !== 'Not running')
+    if (err.message !== 'Not running')
       throw err;
   }
-  if(err) console.warn(err.stack);
+  if (err)
+    console.warn(err.stack);
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,98 +1,198 @@
-(function(){
-'use strict';
+  'use strict';
 
-var fs        = require('fs'),
-    path      = require('path'),
-    config    = require('../config/config.json');
+  var fs = require('fs'),
+      path = require('path'),
+      config = require('../config/config.json'),
+      ActiveDirectory = require('activedirectory');
 
-/**
- * function: Route in Public Filesystem
- */
-function route(route_path, json_obj) {
-  json_obj.path = "/" + route_path;
+  var users = [];
 
-  return function(req, res)
-  {
-    res.get('X-Frame-Options');
-    res.render(route_path, json_obj);
-    res.end();
+module.exports = function(app){
+      console.log("init");
+
+  app.engine('html', require('ejs').renderFile);
+
+  /**
+   * function: Route in Public Filesystem
+   */
+  function route(route_path, json_obj) {
+    console.log("route");
+    json_obj.path = "/" + route_path;
+
+    return function(req, res) {
+      res.get('X-Frame-Options');
+      res.render(route_path, json_obj);
+      res.end();
+    };
+  }
+
+  /**
+   * Authentication (LDAP)
+   */
+  function authenticate(req){
+
+    // if user is already logged in, then route forward
+    for (user in users) {
+      if (user.isEnabled) {
+        return {msg: "isEnabled"};
+      }
+    }
+
+    var cred = { url: config.ldap.url,
+                 baseDN: config.ldap.baseDN,
+                 username: req.username,
+                 password: req.password
+               };
+    // try to bring user input in a form which is accepted by the server
+    try {
+      if (cred.username.split("\\").length > 1) {
+        cred.username = cred.username.split("\\")[1];
+        console.log(cred.username);
+      }
+      if (cred.url.indexOf('ldap:\\\\') == -1) {
+        cred.url = 'ldap:\\\\' + cred.url;
+        console.log(cred.url);
+      }
+      if (cred.username.indexOf("@" + config.ldap.url) == -1) {
+        cred.username += "@" + config.ldap.url;
+        console.log(cred.username);
+      }
+    }
+    catch (err) {
+      console.log("err");
+      return {msg: "ParseError"};
+    }
+
+    var ad = new ActiveDirectory(cred);
+    console.log(JSON.stringify(cred));
+
+    // check, if user exists on server
+    ad.userExists(cred.username, function(err, exists) {
+      if (err) {
+        console.log("exist err ");
+        return {msg: "NotUser"};
+      }
+      // check, if user can authenticate
+      if(exists) {
+        ad.authenticate(cred.username, cred.password, function(err, auth) {
+          if (auth) {
+            console.log("auth");
+            user.push({"username": username, "isEnabled": true})
+            return {msg: "IsEnabled"};
+          }
+          else {
+            console.log("auth err");
+            return {msg: "Failed"};
+          }
+        });
+      }
+    });
   };
+
+  /**
+   * Routes
+   */
+
+  // Defaults
+  app.get('/', function(req, res){
+    console.log("/");
+    res.render(__dirname + '/../public/login/index.html', {title : 'WebVisual Login'});
+  });
+
+  app.get('/index', function(req, res){
+    console.log("index");
+    res.render(__dirname + '/../public/login/index.html', {title : 'WebVisual Login'});
+  });
+
+  /**
+   * Login
+   */
+
+  app.post("/login", function(req, res) {
+
+    console.log(req.body);
+    if(req.body.username && req.body.password) {
+        // check username and password
+        check = authenticate({"username": req.body.username, "password": req.body.password});
+
+        switch (check) {
+          case "IsEnabled":
+             res.redirect(__dirname + '/../public/index.html', {title : 'WebVisual Login'});
+             return;
+          case "NotUser":
+             res.redirect(__dirname + '/../public/login/index.html', {title : 'WebVisual Login'});
+             return;
+          case "IsEnabled":
+             res.redirect(__dirname + '/../public/login/index.html', {title : 'WebVisual Login'});
+             return;
+          case "ParseError":
+             res.redirect(__dirname + '/../public/login/index.html', {title : 'WebVisual Login'});
+             return;
+         }
+     }
+  });
+
+  app.get('/login', function(req, res){
+    console.log("login");
+    res.render(__dirname + '/../public/login/index.html', {title : 'WebVisual Login'});
+  });
+
+  /**
+   * Logout
+   */
+  app.get('/logout', function(req, res){
+    console.log("logout");
+    for (var i = 0; i < users.length; i++) {
+      if (users[i].username == req.username) {
+        users.splice(i-1, 1);
+        break;
+      }
+    }
+    res.render(__dirname + '/../public/login/index.html', {title : 'WebVisual Login'});
+  });
+
+  // Error: Custom 404 page
+  app.use(function(req, res) {
+    res.status(404);
+
+    // Respond with html page
+    if (req.accepts('html')) {
+      res.render('404');
+    }
+  });
+
+  // External Logfile (file path from config.json)
+  app.get('/log', function(req, res){
+    var filepath;
+    if (!config.logs.external_log_path || config.logs.external_log_path == "")
+      filepath =
+          path.resolve(__dirname + '/../../../logs/' + config.logs.external_log);
+    else
+      filepath = path.resolve(config.logs.external_log_path +
+                              config.logs.external_log);
+
+    var text = fs.readFile(filepath, function(err, data) { res.send(data); });
+  });
+
+  // Data File (file path from config.json)
+  app.get(__dirname + '/../public/data', function(req, res){
+    var filepath;
+
+    if (!config.connections.file.path_folder ||
+        config.connections.file.path_folder == "")
+      filepath = path.resolve(__dirname + '/../../../data/' +
+                              config.connections.file.path);
+    else
+      filepath = path.resolve(config.connections.file.path_folder +
+                              config.connections.file.path);
+
+    var text = fs.readFile(filepath, function(err, data) { res.send(data); });
+  });
+
+  // Configuration    (send config.json)
+  app.get('/settings', function(req, res){
+    var filepath = path.resolve(__dirname + '/../../config/config.json');
+    var text = fs.readFile(filepath, function(err, data) { res.send(data); });
+  });
+
 }
-
-/**
- * Routes
- */
-// TODO: Preload through AJAX Custom-Element-Pages especially the Table
-
-// Defaults
-exports.index = route('index', { title: 'WebVisual' });
-
-// External Logfile (file path from config.json)
-exports.externalLogFile = function(req, res) {
-  var filepath;
-  if (!config.logs.external_log_path || config.logs.external_log_path == "")
-    filepath = path.resolve(__dirname + '/../../logs/' + config.logs.external_log);
-  else
-    filepath = path.resolve(config.logs.external_log_path + config.logs.external_log);
-
-  var text = fs.readFile(filepath, function(err, data) {
-    res.send(data);
-  });
-};
-
-// Data File        (file path from config.json)
-exports.dataString = function(req, res) {
-  var filepath;
-
-  if (!config.connections.file.path_folder || config.connections.file.path_folder == "")
-    filepath = path.resolve(__dirname + '/../../data/' + config.connections.file.path);
-  else
-    filepath = path.resolve(config.connections.file.path_folder + config.connections.file.path);
-
-  var text = fs.readFile(filepath, function(err, data) {
-    res.send(data);
-  });
-};
-
-// Configuration    (send config.json)
-exports.settingsJSON = function(req, res) {
-  var filepath = path.resolve(__dirname + '/../config/config.json');
-  var text = fs.readFile(filepath, function(err, data) {
-    res.send(data);
-  });
-};
-
-})();
-
-
-// // EXAMPLE of file routing
-//
-// // Tables
-// // Locals
-// var views    = path.join(__dirname, '../views'),
-//   tables   = path.join(views, 'tables/tables'),
-//   table404 = path.join(tables, '../404_table.jade');
-//
-// //Tables routing
-// exports.table = function(req, res) {
-//   var jadeOpt = {
-//       path: req.url,
-//       title: (config.locals&&config.locals.table&&config.locals.table.title)?
-//           (config.locals.table.title):'Datatable'
-//     },
-//     jadeFile = path.join(tables, 'table')+'.jade';
-//
-//   // Deliver the specified table
-//   if(req.query.type) {
-//     // Modify the jadeFile
-//     jadeFile = path.join(tables, req.query.type)+'.jade';
-//
-//     // Modify the jade object
-//     jadeOpt.type = req.query.type;
-//
-//     // Check if the view exists, otherwise render a 404 message
-//     if(!fs.existsSync(jadeFile)) {
-//       res.status(404);
-//       jadeFile = table404;
-//     }
-//   }
