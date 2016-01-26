37203b752512bd055f84791626846c4ed4d4020a
Fairyon <dagnarus@live.ru>
2016-01-26 16:14:06 +0100

    commented dbcontroller

--- src/data_module/database/lib/dbcontroller.js
+++ src/data_module/database/lib/dbcontroller.js
@@ -7,11 +7,12 @@
  * After that you can call the 'connect(index, callback)' function 
  * for every database.
  * 
- * e.g. dbController.on( 'error', function (err) { console.warn(err) });
- *  dbController.createConnection(config.database, 1); // 1 as db-index
- *  dbController.connect(1, function (err, db_index) { } );
+ * Example:
+ *   dbController.on( 'error', function (err) { console.warn(err) });
+ *   dbController.createConnection(config.database, 1); // 1 as db-index
+ *   dbController.connect(1, function (err, db_index) { } );
  *  
- * To know how the functions work, read the corresponding description.
+ * To know how every function works, read the corresponding description.
  */
 
   // --- Node.js Module dependencies --- //
@@ -36,7 +37,7 @@ DBController.prototype = new EventEmitter;
 
   // --- Local Functions --- //
 
-// Checks if database with given 'index' is registered trough createConnection()
+// Checks if database with given 'index' was registered trough createConnection()
 DBController.prototype.checkIndex = function(index){
   if(this.deviceModels[index] === undefined)
     throw new Error("DeviceModel with index '"+ index +"' doesn't exists!!");
@@ -46,7 +47,7 @@ DBController.prototype.checkIndex = function(index){
  * so you can later connect to it with connect-function. 
  * Refer about possible options to DeviceModel.prototype.createConnection 
  * Important option is the name of the database to connect to.
- *   The function throws Error if there is no 'options'.
+ *   The function throws Error if no 'options' was passed to it.
  *   Another Error will be thrown if database name is not given.
  *    The name can be stored either under 'options.name '
  *    or 'options' can be a String with the name 
@@ -144,22 +145,33 @@ DBController.prototype.close = function(){
 };
 
 /*
- * Remove Model from Controller and make an corresponding 'index' free.
+ * Remove Model from Controller and make the corresponding 'index' free.
  * If no 'index' is given, removes all databases from this Controller.
  * 
- *  callback = function (db_index)
- *             db_index = given 'index'
+ * index: index of the database to be removed, set by createConnection()
+ * callback: function (db_index)
+ *           db_index: given 'index'
  */
 DBController.prototype.remove = function(index, callback){
   if(index !== undefined) delete this.deviceModels[index];
   else this.deviceModels.length = 0; //clear the list
 };
 
-// Creates or updates list of devices in database using given array "devices"
+/*
+ * Create or update list of devices in database using given array "devices"
+ * 
+ * index: index of the database to set the devices, set by createConnection()
+ * devices: array with devices. each 'device' needs to have an id
+ *          e.g. {id: 'device1', type: 'computer'}
+ *          every property you set, can be used in the query function
+ * callback: function(err, model_index)
+ *           err: possible errors
+ *           model_index: index of database, the devices were set
+ */ 
 DBController.prototype.setDevices = function (index, devices, callback) {
   this.checkIndex(index);
   var self=this;
-  // setDevice - is custom function in the dataModel
+  // set devices to the corresponding model
   this.deviceModels[index].setDevices(devices, function(err, model_index){
     if(callback) callback(err, model_index);
   });
@@ -169,13 +181,18 @@ DBController.prototype.setDevices = function (index, devices, callback) {
  * Appends data to Database.
  * Data should look like schema of the dataModel.
  * Calls the callback with possible error and success response
- * newData: can be one Object or Array of Objects
- * callback: function(err, appendedData);
+ * 
+ * index:    index of the database to append the data, set by createConnection()
+ * newData:  can be one Object or Array of Objects
+ * callback: function(err, appendedData, model_index);
+ *           err: possible errors
+ *           appendedData: data, that was appended to the database
+ *           model_index: index of database, the data was appended
  */
 DBController.prototype.appendData = function (index, newData, callback) {
   this.checkIndex(index);
   var self=this;
-  // append - is a custom function in the dataModel
+  // append data to the corresponding database
   this.deviceModels[index].append(newData, function(err, appendedData, model_index){
     if(callback) callback(err, appendedData, model_index);
   });
@@ -183,18 +200,33 @@ DBController.prototype.appendData = function (index, newData, callback) {
 
 /*
  * Search for some query and call the callback with found data
- * How to use the function read by devicedata.js/query
+ * 
+ * index: index of the database to query, set by createConnection()
+ * search_pattern: the search-object. Please read the exact description by devicemodel.js/DeviceModel.prototype.query()
+ * callback: function(err, result, model_index);
+ *           err: possible errors
+ *           result: data, found using the search_pattern
+ *           model_index: index of database, the data was queried
  */
 DBController.prototype.getData = function (index, search_pattern, callback) {
   this.checkIndex(index);
   var self=this;
-  // query - is a custom function in the dataModel
+  // query data from the corresponding database
   this.deviceModels[index].query(search_pattern, function (err, result, model_index) {
     if(callback) callback(err, result, model_index);
   });
 };
+
 /*
- * Sets new fixed size in Megabytes to collection of values for given device id in given model
+ * Set new fixed size in Megabytes to values storage for given device in given model
+ * 
+ * index: index of the database that includes the device to change the size.
+ *        The index need to be set by createConnection()
+ * id: id of the device, to change the size of values storage to
+ * newSize: new fixed size in Megabytes for the values storage of only the given device
+ * callback: function(err, model_index)
+ *           err: possible errors
+ *           model_index: index of database with device, storage size was changed
  */
 DBController.prototype.resize = function (index, id, newSize, callback) {
   this.checkIndex(index);
@@ -206,6 +238,13 @@ DBController.prototype.resize = function (index, id, newSize, callback) {
 
 /*
  * Sets new fixed size in Megabytes to collections of values for all devices in given model
+ * 
+ * index: index of the database that includes the devices to change the size.
+ *        The index need to be set by createConnection()
+ * newSize: new fixed size in Megabytes for the values storage of all devices of given model
+ * callback: function(err, model_index)
+ *           err: possible errors
+ *           model_index: index of database with device, storage size was changed
  */
 DBController.prototype.resizeAllInModel = function (index, newSize, callback) {
   this.checkIndex(index);
@@ -216,7 +255,11 @@ DBController.prototype.resizeAllInModel = function (index, newSize, callback) {
 };
 
 /*
- * Sets new fixed size in Megabytes to collections of values for all devices in given model
+ * Sets new fixed size in Megabytes to collections of values for all devices of all connected databases
+ * 
+ * newSize: new fixed size in Megabytes for the values storage of all devices of all connected databases
+ * callback: function(errors)
+ *           errors: possible errors
  */
 DBController.prototype.resizeAll = function (newSize, callback) {
   var self=this;
@@ -232,10 +275,19 @@ DBController.prototype.resizeAll = function (newSize, callback) {
 
 
 /*
- * Search for some query in tmpDB and call the callback with found data
- * How to use the function read by devicedata.js/query
+ * Search for some query in temporary database and call the callback with found data
+ *   temporary database is the database, that includes only the data, since last tmpDB switch
+ * index: index of the model corresponding to the temporary database
+ *        The index need to be set by createConnection()
+ * tmpDB: reference to the temporary database, that needs to be queried
+ *        you can become the reference from DBController.prototype.switchTmpDB()
+ * search_pattern: the search-object. Please read the exact description by tmpmodel.js/TmpModel.prototype.queryFromModel()
+ * callback: function(err, result, model_index)
+ *           err: possible errors
+ *           result: data, found using the search_pattern
+ *           model_index: index of database, the data was queried
  */
-DBController.prototype.getDataFromTmpModel = function (index, tmpDB, request, callback) {
+DBController.prototype.getDataFromTmpModel = function (index, tmpDB, search_pattern, callback) {
   this.checkIndex(index);
   var self=this;
   this.deviceModels[index].TmpModel.queryFromModel(tmpDB, request, function (err, result, model_index) {
@@ -244,14 +296,18 @@ DBController.prototype.getDataFromTmpModel = function (index, tmpDB, request, ca
 };
 
 /*
- * Switches Temporary Database to the next one 
- * and passes the current one per callback
+ * Switches Temporary Database to the next one and passes the current one per callback
+ * index: index of the model corresponding to the temporary database
+ *        The index need to be set by createConnection()
+ * callback: function(tmpDB, model_index)
+ *           tmpDB: reference to the temporary database
+ *                  needs to be used by DBController.prototype.getDataFromTmpModel
+ *           model_index: index of database, the data was queried
  */
 DBController.prototype.switchTmpDB = function (index, callback) {
   this.checkIndex(index);
   var self=this;
   this.deviceModels[index].switchTmpDB(function(tmpDB, model_index){
-    self.deviceModels[index].tmpDB=tmpDB;
     callback(tmpDB, model_index);
   });
 };
