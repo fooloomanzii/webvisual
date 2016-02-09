052fd79217983c6c47b826e84224b767d6eeb811
fooloomanzii <hannes-b@web.de>
2016-02-09 19:44:40 +0100

    Revert "start to find async problems"
    This reverts commit 7cedcf463dbc753c6703f4f186515d8a81571cd0.

--- src/data_module/clients/client.js
+++ src/data_module/clients/client.js
@@ -1,21 +1,21 @@
-var _ = require('underscore'),
-  default_client_options = require('../defaults/default_client_options.json');
+var _               = require('underscore'),
+default_client_options = require('../defaults/default_client_options.json');
 
 var Client = (function() {
 
-  //Constructor
+//Constructor
   function _Class(socket, options) {
     // Ensure the constructor was called correctly with 'new'
-    if (!(this instanceof _Class)) return new _Class(socket);
+    if( !(this instanceof _Class) ) return new _Class(socket);
 
     this.socket = socket; // User Socket
 
-    if (options == null) options = {};
+    if(options == null) options = {};
     // check if optons has '.patterns'
     _.defaults(options, default_client_options);
 
     // check every pattern for existence of important subobjects
-    options.patterns.map(function(pattern) {
+    options.patterns.map(function(pattern){
       _.defaults(pattern, default_client_options.patterns[0]);
       return pattern;
     });
