34d4d5f5167707088db7b8cb5dd1ab03d49156a0
fooloomanzii <hannes-b@web.de>
2016-06-06 15:24:10 +0200

    small fix

--- build_app.bat
+++ build_app.bat
@@ -1,7 +1,11 @@
 @echo off
 
 echo building webvisual application. Please wait...
-start /WAIT cmd /C "cd %~dp0 & npm.cmd update"
-start /WAIT cmd /C "cd %~dp0 & npm.cmd run dist:win64"
 
+cmd /C "cd %~dp0 & npm.cmd update"
+cmd /C "cd %~dp0 & npm.cmd run dist:win"
+
+echo building webvisual successful..
+echo you can close this window now
 pause
+/WAIT
