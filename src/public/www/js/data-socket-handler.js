b148c794b733e20ed8e3b06a564634e81dd625d2
fooloomanzii <hannes-b@web.de>
2016-09-08 21:35:11 +0200

    class structure

--- src/public/www/js/data-socket-handler.js
+++ src/public/www/js/data-socket-handler.js
@@ -16,15 +16,13 @@ function DataSocketHandler(socketName, name) {
     query: "name=" + name
   });
 
-  var self = this;
-
   // Connect
   this.socket.on('connect', function() {
     console.info("client connected to: " + window.location.host);
   });
   // Init connection
-  this.socket.on('initByServer', function(settings) {
-    if (self.opened === false) {
+  this.socket.on('initByServer', (function(settings) {
+    if (this.opened === false) {
       window.Groups = settings.groups;
       window.GroupingKeys = settings.groupingKeys;
       window.PreferedGroupingKeys = settings.preferedGroupingKeys;
@@ -35,34 +33,34 @@ function DataSocketHandler(socketName, name) {
           window.Content[label][id].nodes = [];
         }
       if (settings.svg)
-        self._loadSvgSources(settings.svg);
+        this._loadSvgSources(settings.svg);
     } else {
-      self.connect(window.selectedLabels);
+      this.connect(window.selectedLabels);
     }
-  });
+  }).bind(this));
   // Receive Data
-  this.socket.on('initial', function(message) {
-    if (message) {
-      self._update(message);
+  this.socket.on('initial', (function(message) {
+    if (message !== undefined) {
+      this._update(message);
       // select the MainPage
       window.PageSelector = document.querySelector('neon-animated-pages#PageSelector');
       window.PageSelector.select("1");
       // Set Window title to selectedLabels
       document.title = ((window.selectedLabels.length > 1) ? window.selectedLabels.join(', ') : window.selectedLabels.toString());
-      self.opened = true;
+      this.opened = true;
     }
     else
       console.info("socket: received empty message");
     return;
-  });
+  }).bind(this));
   // Receive Data
-  this.socket.on('update', function(message) {
+  this.socket.on('update', (function(message) {
     if (message)
-      self._update(message);
+      this._update(message);
     else
       console.info("socket: received empty message");
     return;
-  });
+  }).bind(this));
   // Disconnect
   this.socket.on('disconnect', function() {
     console.warn("client disconnected to: " + window.location.host);
@@ -78,131 +76,140 @@ function DataSocketHandler(socketName, name) {
 }
 
 // MESSAGE HANDLING
-DataSocketHandler.prototype.connect = function connect(labels) {
-  // send Config
-  if (!labels || labels.length === 0) {
-    console.warn("No labels set for connection");
-    return;
-  }
-  this._init();
-  // setTimeout( function() {
-    this.socket.emit('initByClient', {
-      labels: labels
-    })
-  // }, 5000);
-}
-
-DataSocketHandler.prototype.disconnect = function disconnect() {
-  if (socket) {
-    this.socket.disconnect();
-    this.opened = false;
-  }
-}
-
-DataSocketHandler.prototype._init = function _init() {
-  this._getContentNodes();
-}
-
-DataSocketHandler.prototype._getContentNodes = function _getContentNodes() {
-  // grouping updatable window.Content in one Object
-  if (window.Content === undefined)
-    window.Content = {};
-  var updatable = document.querySelectorAll(Selector);
-  var label = '',
-    id = '';
-  for (var i = 0; i < updatable.length; i++) {
-    label = updatable[i].getAttribute('label');
-    id = updatable[i].getAttribute('id');
-    if (label && id) {
-      if (!window.Content[label])
-        window.Content[label] = {};
-      if (!window.Content[label][id])
-        window.Content[label][id] = {nodes: [], values: [], database: new Database(label, id)};
-      if (window.Content[label][id].nodes.indexOf(updatable[i]) === -1)
-        window.Content[label][id].nodes.push(updatable[i]);
+DataSocketHandler.prototype = {
+  constructor: DataSocketHandler,
+  connect: function (labels) {
+    // send Config
+    if (!labels || labels.length === 0) {
+      console.warn("No labels set for connection");
+      return;
     }
-  }
-  updatable.length = 0;
-}
-
-DataSocketHandler.prototype._loadSvgSources = function _loadSvgSources(sources) {
-  var xhttp = [];
-  var req;
-  for (var label in sources) {
-    for (var id in sources[label]) {
-      // import svg
-      // prevents multible reloading after creation time
-      if (!sources[label][id].src || (SvgSource[id] && SvgSource[id].src && SvgSource[id].src === sources[label][id].src))
-        continue;
-      window.SvgSource[id] = {
-        selectable: sources[label][id].selectable,
-        src: sources[label][id].src
-      };
-      req = new XMLHttpRequest();
-      req.onreadystatechange = function(index) {
-        if (this.readyState === 4 && this.status === 200) {
-          window.SvgSource[index].node = this.responseXML.documentElement;
-        }
-      }.bind(req, id);
-      req.open("GET", sources[label][id].src, true);
-      req.send();
-      xhttp.push(req);
+    this._init();
+    // setTimeout( function() {
+      this.socket.emit('initByClient', {
+        labels: labels
+      })
+    // }, 5000);
+  },
+  disconnect: function () {
+    if (socket) {
+      this.socket.disconnect();
+      this.opened = false;
     }
-  }
-}
-
-DataSocketHandler.prototype._update = function _update(message) {
-  if (Array.isArray(message)) // if message is an Array
-    for (var i = 0; i < message.length; i++)
-      this._updateContent(message[i]);
-  else // if message is a single Object
-    this._updateContent(message);
-}
-
-DataSocketHandler.prototype._updateContent = function _updateContent(message) {
-  if (!message.content)
-    return;
-
-  var label = message.label;
-  var len, spliced;
-
-  for (var id in message.content) {
-    len = message.content[id].values.length;
-    spliced = [];
-
-    if (len > maxValues)
-      message.content[id].values = message.content[id].values.slice(len-maxValues, len);
-    // console.log(len, label, id);
-    // message.content[i].values.forEach(function(d) {
-    //   d.x = Date.parse(d.x); // parse Date in Standard Date Object
-    // });
-
-    if (window.Content[label][id].values.length === 0)
-      window.Content[label][id].values = message.content[id].values;
-    else
-      for (var j = message.content[id].values.length - 1; j >= 0 ; j--) {
-        window.Content[label][id].values.push(message.content[id].values[j]);
+  },
+  _init: function () {
+    this._getContentNodes();
+  },
+  _getContentNodes: function () {
+    // grouping updatable window.Content in one Object
+    if (window.Content === undefined)
+      window.Content = {};
+    var updatable = document.querySelectorAll(Selector);
+    var label = '',
+      id = '';
+    for (var i = 0; i < updatable.length; i++) {
+      label = updatable[i].getAttribute('label');
+      id = updatable[i].getAttribute('id');
+      if (label && id) {
+        if (!window.Content[label])
+          window.Content[label] = {};
+        if (!window.Content[label][id])
+          window.Content[label][id] = {nodes: [], values: [], database: new Database(label, id)};
+        if (window.Content[label][id].nodes.indexOf(updatable[i]) === -1)
+          window.Content[label][id].nodes.push(updatable[i]);
       }
-    if (window.Content[label][id].values.length > maxValues) {
-      spliced = window.Content[label][id].values.splice(0, window.Content[label][id].values.length - maxValues);
     }
+    updatable.length = 0;
+  },
+  _loadSvgSources: function (sources) {
+    var xhttp = [];
+    var req;
+    for (var label in sources) {
+      for (var id in sources[label]) {
+        // import svg
+        // prevents multible reloading after creation time
+        if (!sources[label][id].src || (SvgSource[id] && SvgSource[id].src && SvgSource[id].src === sources[label][id].src))
+          continue;
+        window.SvgSource[id] = {
+          selectable: sources[label][id].selectable,
+          src: sources[label][id].src
+        };
+        req = new XMLHttpRequest();
+        req.onreadystatechange = function(index) {
+          if (this.readyState === 4 && this.status === 200) {
+            window.SvgSource[index].node = this.responseXML.documentElement;
+          }
+        }.bind(req, id);
+        req.open("GET", sources[label][id].src, true);
+        req.send();
+        xhttp.push(req);
+      }
+    }
+  },
+  _update: function (message) {
+    if (Array.isArray(message)) // if message is an Array
+      for (var i = 0; i < message.length; i++)
+        this._updateContent(message[i]);
+    else // if message is a single Object
+      this._updateContent(message);
+  },
+  _updateContent: function (message) {
+    if (!message.content)
+      return;
 
-    window.Content[label][id].database.add(message.content[id].values);
+    var label = message.label;
+    var len, spliced;
 
-    if (window.Content[label][id]) {
+    for (var id in message.content) {
+      if (window.Content[label] === undefined || window.Content[label][id] === undefined) {
+        console.warn("no Content-Object for", label, id); continue;
+      }
+      len = message.content[id].values.length;
+      spliced = [];
+
+      if (len > maxValues)
+        message.content[id].values = message.content[id].values.slice(len-maxValues, len);
+      // console.log(len, label, id);
+      // message.content[i].values.forEach(function(d) {
+      //   d.x = Date.parse(d.x); // parse Date in Standard Date Object
+      // });
+
+      if (window.Content[label][id].values.length === 0)
+        window.Content[label][id].values = message.content[id].values;
+      else
+        for (var j = message.content[id].values.length - 1; j >= 0 ; j--) {
+          window.Content[label][id].values.push(message.content[id].values[j]);
+        }
+      if (window.Content[label][id].values.length > maxValues) {
+        spliced = window.Content[label][id].values.splice(0, window.Content[label][id].values.length - maxValues);
+      }
+
+      window.Content[label][id].database.add(message.content[id].values);
+
       for (var j = 0; j < window.Content[label][id].nodes.length; j++) {
         window.Content[label][id].nodes[j].insertValues(message.content[id].values);
         if (spliced.length > 0)
           window.Content[label][id].nodes[j].spliceValues({start: 0, length: spliced.length, values: spliced});
       }
-    } else {
-      console.warn("no window.Content for", label, id);
-    }
-  }
-}
 
-DataSocketHandler.prototype.compareFn = function compareFn(a, b) {
-  if (a.x > b.x) return 1;
-  if (a.x < b.x) return -1;
-  return 0;
+      // window.Content[label][id].database.last(console.log);
+    }
+    window.Content.Lakeshore.id1.database.last(( function(max) {
+      console.log('x last', new Date(max.x));
+    }).bind(this));
+    window.Content.Lakeshore.id1.database.min(( function(max) {
+      console.log('y min', max.y);
+    }).bind(this), 'y');
+    window.Content.Lakeshore.id1.database.first(( function(max) {
+      console.log('x first', new Date(max.x));
+    }).bind(this));
+    window.Content.Lakeshore.id1.database.max(( function(max) {
+      console.log('y max', max.y);
+    }).bind(this), 'y');
+  },
+  compareFn: function (a, b) {
+    if (a.x > b.x) return 1;
+    if (a.x < b.x) return -1;
+    return 0;
+  }
 }
