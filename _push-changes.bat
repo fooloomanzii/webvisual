3b07c82f983eb17ccb931ac374497befa876fcf1
fooloomanzoo <hannes-b@web.de>
2016-10-27 06:00:43 +0200

    small fixes

--- _push-changes.bat
+++ _push-changes.bat
@@ -2,9 +2,10 @@
 
 echo Performing Git Operations. This can take a moment ...
 git add .
+echo.
 echo Enter a Message for the Commit
 set /p CommitMessage=
-echo %CommitMessage%
+echo.
 git commit -m "%CommitMessage%"
 echo.
 git push
