c5ab968ae87700e03d6f37f9ef6ecaa9fb62c38b
fooloomanzii <hannes-b@web.de>
2016-10-27 10:05:06 +0200

    int tests

--- _push.bat
+++ _push.bat
@@ -2,12 +2,13 @@
 
 echo Performing Git Operations. This can take a moment ...
 git submodule foreach "git add ."
-git add .
 echo.
 echo Enter a Message for the Commit
 set /p CommitMessage=
 echo.
 git submodule foreach "git commit -m '%CommitMessage%'"
+echo.
+git add .
 git commit -m "%CommitMessage%"
 echo.
 git push --recurse-submodules=on-demand
