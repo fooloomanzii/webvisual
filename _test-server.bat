b4753d768bb50919aca8f56b430218147ebe4b02
fooloomanzii <hannes-b@web.de>
2016-12-06 23:48:21 +0100

    test site & demo

--- _test-server.bat
+++ _test-server.bat
@@ -1,7 +1,7 @@
 @echo off
-echo Start Simple HTTP Server in /src/public/www ...
+echo Start Simple HTTP Server in /src/server/views ...
 
 start chrome --new-window http://localhost:8000
-cmd /C "cd %~dp0/src/public/www & python -m http.server 8000"
+cmd /C "cd %~dp0/src/server/views & python -m http.server 8000"
 
 pause
