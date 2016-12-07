e20bc13832e87fd2177708770270dc84c1d56fe3
fooloomanzoo <hannes-b@web.de>
2016-12-07 04:43:29 +0100

    renaming, style

--- _test-server.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Start Simple HTTP Server in /src/server/views ...
-
-start chrome --new-window http://localhost:8000
-cmd /C "cd %~dp0/src/server/views & python -m http.server 8000"
-
-pause
