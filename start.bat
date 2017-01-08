379991462b47c4dabf7598593674118fd1ddbd70
fooloomanzoo <hannes-b@web.de>
2017-01-08 09:39:33 +0100

    redis client

--- start.bat
+++ start.bat
@@ -6,6 +6,6 @@ echo starting Databases
 cmd /C "cd %~dp0/ & start_db_session_manager.bat"
 
 echo Server is launching. Please wait...
-cmd /C "cd %~dp0/src & electron ."
+cmd /C "cd %~dp0/src & npm.cmd start"
 
 pause
